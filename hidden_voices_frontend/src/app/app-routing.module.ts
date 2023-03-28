5f1ba68e848e70081917a5a62bfef9651309156b
Anuraag <anuraag.k.shankar@gmail.com>
2023-03-28 21:04:17 +0530

    Removed wildcard path

--- hidden_voices_frontend/src/app/app-routing.module.ts
+++ hidden_voices_frontend/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { ScrapeComponent } from './features/scrape/scrape.component';
 const routes: Routes = [
   { path: 'scrape', component: ScrapeComponent },
   { path: 'generate', component: GenerateComponent },
-  { path: '**', redirectTo: '/scrape', pathMatch: 'full' },
 ];
 
 @NgModule({
