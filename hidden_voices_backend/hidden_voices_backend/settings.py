91622ef93b462100e5cd3e7fbc985413d245ce66
Anuraag Shankar <anuraag.k.shankar@gmail.com>
2024-01-31 23:00:59 +0530

    Added Werkzeug.

--- hidden_voices_backend/hidden_voices_backend/settings.py
+++ hidden_voices_backend/hidden_voices_backend/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'werkzeug_debugger_runserver',
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
